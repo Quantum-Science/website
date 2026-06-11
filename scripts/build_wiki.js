31110e64f5a2fba305476caea213c964b441b2db
katsumi143 <32640219+katsumi143@users.noreply.github.com>
2026-06-12 00:02:32 +1000

    feat: Basic image gallery interactivity

--- scripts/build_wiki.js
+++ scripts/build_wiki.js
@@ -7,4 +7,4 @@ const [layout_path, routes_path, wiki_path, base_page] = setup(is_dev);
 const files = readdirSync(wiki_path, { recursive: true })
 	.filter(entry => entry.endsWith('.md'));
 for (const entry of files)
-	compile_route(entry, wiki_path, routes_path, base_page, is_dev);
+	await compile_route(entry, wiki_path, routes_path, base_page, is_dev);

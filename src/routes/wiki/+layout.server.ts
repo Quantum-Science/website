cc7bd516f73f22d1a499fe43eb3d48565288bd2b
katsumi143 <32640219+katsumi143@users.noreply.github.com>
2026-09-05 18:08:29 +1000

    feat(wiki): Remove page access lock

--- src/routes/wiki/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import { error } from '@sveltejs/kit';
-
-import { PUBLIC_ENABLE_WIKI } from '$env/static/public';
-export function load() {
-	if (PUBLIC_ENABLE_WIKI !== 'true')
-		return error(404, 'Access to the wiki is currently restricted at this time.');
-}
